4a6419c6ff3d69acf1245c9ce8a583d1d4df4339
jihun-k <k961118@snu.ac.kr>
2021-01-15 14:49:08 +0900

    Add tensorboard

--- src/train.py
+++ src/train.py
@@ -1,47 +1,31 @@
-from config import ROOT_DIR
 import torch
+import torchvision
 import os
 
+from config import ROOT_DIR
 from data import coco
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 def main():
     ''' main '''
-    # create dataloader
-    imgpath = os.path.join(ROOT_DIR, "datasets", "COCO", "train2017")
-    jsonpath = os.path.join(ROOT_DIR, "datasets", "COCO", "annotations", "instances_train2017.json")
-    dataset = coco.CocoDataset(imgpath, jsonpath)
-
-    '''
-    image, boxes, segments = dataset[0]
-    image_tensor = torch.tensor(image)
-    print(image_tensor.size())
-    print(boxes)
-    print(segments)
-    '''
-
-    '''
+    # tensorboard writer
     writer = SummaryWriter()
 
-    x = torch.arange(-5, 5, 0.1).view(-1, 1)
-    y = -5 * x + 0.1 * torch.randn(x.size())
+    # create dataloader
+    imgpath = os.path.join(ROOT_DIR, "datasets", "COCO", "val2017")
+    jsonpath = os.path.join(ROOT_DIR, "datasets", "COCO", "annotations", "instances_val2017.json")
 
-    model = torch.nn.Linear(1, 1)
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
+    train_set = coco.CocoDataset(imgpath, jsonpath)
+    train_loader = DataLoader(train_set, batch_size=1)
 
-    def train_model(iter):
-        for epoch in range(iter):
-            y1 = model(x)
-            loss = criterion(y1, y)
-            writer.add_scalar("Loss/train", loss, epoch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-    train_model(1000)
-    writer.flush()
-    '''
+    for i, (image, boxes, segments) in enumerate(train_loader):
+        grid = torchvision.utils.make_grid(image)
+        writer.add_image("image/show", grid, 0)
+        # writer.flush()
+        break
+        
 
 if __name__ == "__main__":
     main()
